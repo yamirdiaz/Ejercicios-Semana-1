de5c9a581da98275d70ca5fac7652289b30f74fb
yamirdiaz <yamir7diaz@gmail.com>
2025-07-15 15:00:41 -0400

    Step 3: Agregar the breakpoints and functonality.
    Step 3.1: Add media querys to different screen and modify the behavior of the Body and main element.
    
    Step 3.2: Creacion de la funcion Handlereset la cual efectua el llamado de la API y guarda los valores en el state.
    
    Step 3.3: Sustituir los datos de prueba con los valores generados por la  API.
    
    Step 3.4: Utilizar el render condicional para asigar valores cuando la API no ha respondido.
    
    Step 3.5: Implementar la funcion de llamado de la API dentro de Useeffect para la implementacion de la function en el cliclo mount.

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -1,39 +1,73 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import FontAwesomeLibrary from './icons/icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css'
+import { faL } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 
 function App() {
+  const [ userInfo, setUserInfo ] = useState({})
+  const [ isWomen, setIsWomen ] = useState(true)
+  const bgGenderColor = isWomen ? "border-pink-300" : "border-blue-500"
+  const textGenderColor = isWomen ? "text-pink-300" : "text-blue-500"
+
+    const handleRefresh = useCallback(async() => {
+      try {
+        const response = await axios.get('https://randomuser.me/api/')
+        const data = response.data.results[0]
+        console.log(data.gender)
+        const date =new Date(data.dob.date)
+        setUserInfo({
+          "gender" : data.gender,
+          "name": `${data.name.first} ${data.name.last}`,
+          "location": `${data.location.state} ${data.location.country}`,
+          "email": data.email,
+          "birthday": date.toLocaleDateString(),
+          "phone": date.phone,
+          "image": data.picture.large
+        })
+
+        setIsWomen(data.gender === "female" ? true : false)
+      } catch(err) {
+        console.error("An error has ocurred: " + err)
+      }
+    }
+  )
+
+  useEffect(() => {
+    handleRefresh
+  }, [handleRefresh])
+
   
 
   return (
     <>
-      <header className={`bg-gray-700 text-center p-4 rounded-lg text-sky-100`}>
-        <img className={`w-40 rounded-full mx-auto border-6 border-dotted border-pink-300`} src='https://randomuser.me/api/portraits/women/44.jpg'></img>
+      <header className={`bg-gray-700 text-center p-4 rounded-lg text-sky-100 md:w-3/5`}>
+        <img className={`w-40 rounded-full mx-auto border-6 border-dotted ${bgGenderColor} lg:w-60`} src={userInfo.image || "https://randomuser.me/api/portraits/women/32.jpg"}></img>
         <div>
           <p >Hi, my name is...</p>
-          <p className='text-pink-300 text-4xl my-8 '>(Samira)</p>
+          <p className={`${textGenderColor} text-4xl my-8 lg:text-6xl`}>{ userInfo.name || "Samira"}</p>
         </div>
-        <button className='bg-sky-100 text-gray-700 p-4 w-3/5 font-mono font-bold text-xl rounded-xl hover:shadow-md hover:shadow-red-500 hover:cursor-pointer'>Refresh</button>
+        <button onClick={handleRefresh} className='bg-sky-100 text-gray-700 p-4 w-3/5 font-mono font-bold text-xl rounded-xl hover:shadow-md hover:shadow-red-500 hover:cursor-pointer lg:w-full'>Refresh</button>
       </header>
-      <main className={`bg-gray-700 text-sky-100 rounded-lg p-4 my-4`}>
+      <main className={`bg-gray-700 text-sky-100 rounded-lg p-4 my-4 md:2/5 md:my-0`}>
         <h2 className='font-mono font-bold text-xl text-center underline my-8'>Here's more about me:</h2>
-        <div className='flex flex-wrap justify-center gap-4'>
+        <div className='flex flex-wrap justify-center gap-4 md:flex-col md:items-center'>
           <section className='flex items-center gap-x-2'>
             <FontAwesomeIcon icon="fa-solid fa-envelope" className='text-xl text-rose-500'/>
-            <p>(example@gmail.com)</p>
+            <p>{ userInfo.email ||" example@gmail.com"}</p>
           </section>
           <section className='flex items-center gap-x-2 mx-auto'>
             <FontAwesomeIcon icon="fa-solid fa-map-location-dot" className='text-xl text-green-500' />
-            <p>(London, UK)</p>
+            <p>{ userInfo.location || "London, UK"}</p>
           </section>
           <section className='flex items-center gap-x-2'>
             <FontAwesomeIcon icon="fa-solid fa-phone" className='text-xl text-violet-500' />
-            <p>(+1-849-678-971)</p>
+            <p>{ userInfo.phone || "+1-849-678-971"}</p>
           </section>
           <section className='flex items-center gap-x-2'>
             <FontAwesomeIcon icon="fa-solid fa-cake-candles" className='text-xl text-amber-500' />
-            <p>(04/05/1960)</p>
+            <p>{ userInfo.birthday || "04/05/1960"}</p>
           </section>
         </div>
       </main>
